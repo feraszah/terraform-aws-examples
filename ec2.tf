44c1633d91360da98618d44d491cad6a46aa01e5
Zahid <P2856504@CORP.CHARTERCOM.COM>
2019-01-02 14:53:25 -0700

    Adding first file to repo

---
+++ ec2.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  access_key = "ACCESS_KEY_HERE"
+  secret_key = "SECRET_KEY_HERE"
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-2757f631"
+  instance_type = "t2.micro"
+}
+
+# Obtain access/secret key through your IAM role credentials 
